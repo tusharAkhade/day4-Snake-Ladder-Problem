2ad7df1aa4bd6c3953138be68c5bde88099a7dbe
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-14 02:04:10 +0530

    Added: Die roll to get number between 1 and 6

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # day4-Snake-Ladder-Problem
-## Use Case 1
-    Initializing player position at zero.
+## Use Case 2
+    Player rolls the die to get a number between 1 to 6 using random() method.
--- snakeLadder.java
+++ snakeLadder.java
@@ -1,7 +1,7 @@
-public class UC1 {
+public class UC2 {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder simulator");
-		int playerPosition = 0;	
-		System.out.println("Starting position is " + playerPosition );
+		int playerPosition = 0;
+		int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		System.out.println("Dice roll: " + die);		
 	}
 }
