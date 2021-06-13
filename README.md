4c1354e7984511ecce839bbdb5df6f5b0dd17ee3
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-14 01:58:39 +0530

    Added: player position initialization code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # day4-Snake-Ladder-Problem
+## Use Case 1
+    Initializing player position at zero.
---
+++ snakeLadder.java
@@ -0,0 +1,7 @@
+public class UC1 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder simulator");
+		int playerPosition = 0;	
+		System.out.println("Starting position is " + playerPosition );
+	}
+}
